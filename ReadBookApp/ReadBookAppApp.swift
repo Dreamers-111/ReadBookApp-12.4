ccab1712d7f2273791e9f375ae0fbb6fe2591a31
DuongXuanHuy-207PM68593 <alexandra89610@gmail.com>
2022-03-04 00:16:11 +0700

    Initial Commit

---
+++ ReadBookApp/ReadBookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadBookAppApp.swift
+//  ReadBookApp
+//
+//  Created by Duong Huy on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ReadBookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
